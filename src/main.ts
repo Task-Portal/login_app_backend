7e95bcafbd00fcd0bc7c3a8e9d49e9053b4b01b5
Alex <lion1987.fm@gmail.com>
2025-09-24 10:23:00 +0300

    Added validation to classes

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,15 @@ import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { FILE_UPLOADS_DIR } from './constants';
 import { appConfig } from './config/app.config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     cors: true,
   });
   app.useStaticAssets(join(__dirname, '..', FILE_UPLOADS_DIR));
-  await app.listen(appConfig.port!);
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(appConfig.port);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,11 @@
+import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+
 export class CreateUserDto {
+  @IsNotEmpty()
   username: string;
+  @IsEmail()
   email: string;
+  @IsNotEmpty()
+  @MinLength(8, { message: 'Password must be at least 8 characters long. ' })
   password: string;
 }
